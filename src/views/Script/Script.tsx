d2316bca68115a8fef61bfabcbf9a2e6297f47b8
crqch <48185508+crqch@users.noreply.github.com>
2026-04-19 16:25:30 +0200

    lint: remove unused variable

--- src/views/Script/Script.tsx
+++ src/views/Script/Script.tsx
@@ -11,7 +11,7 @@ const Script = () => {
         children={script}
         remarkPlugins={[remarkGfm]}
         components={{
-          code({ node, className, children, ...props }) {
+          code({ className, children, ...props }) {
             const match = /language-(\w+)/.exec(className || "");
             return match ? (
               <SyntaxHighlighter PreTag="div" language="python">
