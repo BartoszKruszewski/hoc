e7b61842d9903e55aa4fedb62bdf2fbba9d33bc0
crqch <48185508+crqch@users.noreply.github.com>
2026-04-19 16:25:13 +0200

    refactor: remove empty files

--- src/views/Tasks/TaskPanel/TaskPanel.test.tsx
+++
@@ -1,8 +0,0 @@
-// import ReactDOM from 'react-dom';
-// import TaskPanel from './TaskPanel';
-
-// it('It should mount', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<TaskPanel />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
